50710dd9818e172dff6bd7f82ae18a4431fb36aa
steve <dsgipe@gmail.com>
2018-07-11 15:17:41 -0600

    http security

---
+++ src/main/java/io/pivotal/pal/tracker/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private boolean envHttpsDisabled = false;
+
+    public SecurityConfiguration (@Value("${HTTPS_DISABLED}") Boolean envHttpsDisabled) {
+        this.envHttpsDisabled = envHttpsDisabled;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+        .inMemoryAuthentication()
+        .withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (envHttpsDisabled) {
+            http.authorizeRequests().antMatchers("/**").hasRole("USER")
+                    .and().httpBasic()
+                    .and().csrf().disable();
+        } else {
+            http.authorizeRequests().antMatchers("/**").hasRole("USER")
+                    .and().httpBasic()
+                    .and().requiresChannel().anyRequest().requiresSecure()
+                    .and().csrf().disable();
+        }
+    }
+}
